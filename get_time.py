1b498d7de6942055d750e7e2b4b08f2481f23453
Darrell Lim <96804398+dlimtx@users.noreply.github.com>
2025-10-26 12:32:06 +0800

    Refactor get_time function to include timezone adjustment

--- get_time.py
+++ get_time.py
@@ -2,7 +2,7 @@ import datetime as dt
 
 
 def get_time():
-    time_now = dt.datetime.now()
+    time_now = dt.datetime.now() + timedelta(hours=8)
     year_now = str(time_now.year)
     month_now = time_now.month
     if month_now < 10:
@@ -53,37 +53,3 @@ def get_time():
 
     time_list = [year_now, month_now, day_now, hour_now, minute_split]
     return time_list
-
-
-# def get_previous_time():
-#     time_previous = dt.datetime.now() - dt.timedelta(minutes=5)
-#     year_previous = str(time_previous.year)
-#     month_previous = time_previous.month
-#     if month_previous < 10:
-#         month_previous = f"0{month_previous}"
-#     else:
-#         month_previous = str(month_previous)
-#     day_previous= time_previous.day
-#     if day_previous < 10:
-#         day_previous = f"0{day_previous}"
-#     else:
-#         day_previous = str(day_previous)
-#     hour_previous = time_previous.hour
-#     if hour_previous < 10:
-#         hour_previous = f"0{hour_previous}"
-#     else:
-#         hour_previous = str(hour_previous)
-#     minute_previous = time_previous.minute
-#     minute_previous_split = []
-#     while minute_previous > 0:
-#         minute_previous, rmd = divmod(minute_previous, 10)
-#         minute_previous_split.append(rmd)
-#         if time_previous.minute < 10:
-#             minute_previous_split.append(0)
-#     if minute_previous_split[0] > 6:
-#         minute_previous_split[0] = 5
-#     else:
-#         minute_previous_split[0] = 0
-#
-#     time_previous_list = [year_previous, month_previous, day_previous, hour_previous, minute_previous_split]
-#     return time_previous_list
